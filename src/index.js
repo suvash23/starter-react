4a4ec57d0efce7702e1cd66c21e5a277013f5816
Suvash Sarker <suvash.cse.duet@gmail.com>
2021-06-23 01:40:03 +0600

    Showing custom children

--- src/index.js
+++ src/index.js
@@ -2,40 +2,54 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Faker from 'faker';
 import CommentDetail from './CommentDetail';
-
+import ApprovalCard from './ApprovalCard';
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail
-                author="Sam"
-                content="Nice Blog Post One!"
-                timeAgo="Today at 2:00PM"
-                avatar={Faker.image.avatar()}
-            />
-            <CommentDetail
-                author="John"
-                content="Nice Blog Post Two!"
-                timeAgo="Today at 3:00PM"
-                avatar={Faker.image.avatar()}
-            />
-            <CommentDetail
-                author="Ricky"
-                content="Nice Blog Post Three!"
-                timeAgo="Today at 4:00PM"
-                avatar={Faker.image.avatar()}
-            />
-            <CommentDetail
-                author="Eady"
-                content="Nice Blog Post Four!"
-                timeAgo="Today at 5:00PM"
-                avatar={Faker.image.avatar()}
-            />
-            <CommentDetail
-                author="Jeff"
-                content="Nice Blog Post Five!"
-                timeAgo="Today at 6:00PM"
-                avatar={Faker.image.avatar()}
-            />
+            <ApprovalCard>
+                <CommentDetail
+                    author="Sam"
+                    content="Nice Blog Post One!"
+                    timeAgo="Today at 2:00PM"
+                    avatar={Faker.image.avatar()}
+                />
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail
+                    author="John"
+                    content="Nice Blog Post Two!"
+                    timeAgo="Today at 3:00PM"
+                    avatar={Faker.image.avatar()}
+                />
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail
+                    author="Ricky"
+                    content="Nice Blog Post Three!"
+                    timeAgo="Today at 4:00PM"
+                    avatar={Faker.image.avatar()}
+                />
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail
+                    author="Eady"
+                    content="Nice Blog Post Four!"
+                    timeAgo="Today at 5:00PM"
+                    avatar={Faker.image.avatar()}
+                />
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail
+                    author="Jeff"
+                    content="Nice Blog Post Five!"
+                    timeAgo="Today at 6:00PM"
+                    avatar={Faker.image.avatar()}
+                />
+            </ApprovalCard>
         </div>
     );
 }
