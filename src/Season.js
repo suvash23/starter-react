7e27b666d759df9e4ddb5d6d04caf758359b15f7
Suvash Sarker <suvash.cse.duet@gmail.com>
2021-07-06 01:49:57 +0600

    Season display - class, state, props

---
+++ src/Season.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Season = () => {
+    return (
+    <div>Hello Season!</div>
+    );
+}
+
+ReactDOM.render(<Season />, document.querySelector('#root'));
--- src/index.js
+++ src/index.js
@@ -1,57 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Faker from 'faker';
-import CommentDetail from './CommentDetail';
-import ApprovalCard from './ApprovalCard';
-const App = () => {
-    return (
-        <div className="ui container comments">
-            <ApprovalCard>
-                <CommentDetail
-                    author="Sam"
-                    content="Nice Blog Post One!"
-                    timeAgo="Today at 2:00PM"
-                    avatar={Faker.image.avatar()}
-                />
-            </ApprovalCard>
-
-            <ApprovalCard>
-                <CommentDetail
-                    author="John"
-                    content="Nice Blog Post Two!"
-                    timeAgo="Today at 3:00PM"
-                    avatar={Faker.image.avatar()}
-                />
-            </ApprovalCard>
-
-            <ApprovalCard>
-                <CommentDetail
-                    author="Ricky"
-                    content="Nice Blog Post Three!"
-                    timeAgo="Today at 4:00PM"
-                    avatar={Faker.image.avatar()}
-                />
-            </ApprovalCard>
-
-            <ApprovalCard>
-                <CommentDetail
-                    author="Eady"
-                    content="Nice Blog Post Four!"
-                    timeAgo="Today at 5:00PM"
-                    avatar={Faker.image.avatar()}
-                />
-            </ApprovalCard>
-
-            <ApprovalCard>
-                <CommentDetail
-                    author="Jeff"
-                    content="Nice Blog Post Five!"
-                    timeAgo="Today at 6:00PM"
-                    avatar={Faker.image.avatar()}
-                />
-            </ApprovalCard>
-        </div>
+import React from "react";
+import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
+class App extends React.Component {
+  state = { lat: null, errorMessage: "" };
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        // we called setstate!!!!
+        this.setState({ lat: position.coords.latitude });
+      },
+      (err) => {
+        this.setState({ errorMessage: err.message });
+      }
     );
+  }
+  componentDidUpdate() {
+    console.log("componentDidUpdate");
+  }
+  renderContent() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat} />;
+    }
+
+    return <Spinner message="Please alow location request!" />;
+  }
+  render() {
+    <div>{this.renderContent()}</div>;
+  }
 }
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector("#root"));
