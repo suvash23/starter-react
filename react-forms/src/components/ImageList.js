8bc97280c3509103e36081e545457685caaa9d97
Suvash Sarker <suvash.cse.duet@gmail.com>
2021-08-19 00:23:24 +0600

    code clean

--- react-forms/src/components/ImageList.js
+++ react-forms/src/components/ImageList.js
@@ -6,7 +6,6 @@ const ImageList = (props) => {
   console.log(props.images);
   const images = props.images.map((image) => {
     return <ImageCard key={image.id} image={image} />;
-    //return <img key={id} src={urls.regular} alt={description} />;
   });
   return <div className="image-list">{images}</div>;
 };
